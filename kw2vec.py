b53311bc68f82aaa6112fb2111d3065ec167dce0
difonjohaiv <edison0824@outlook.com>
2023-07-19 17:29:59 +0800

    0

--- kw2vec.py
+++ kw2vec.py
@@ -49,7 +49,7 @@ def build_view(save_path, kw_embedding_list):
                 news_02 = kw_embedding_list[j]
                 score = cosine_similarity(news_01, news_02)
                 sum = np.sum(score[score > 0.6])
-                if sum >= 1.8:
+                if sum >= 3.0:
                     writer.writerow([i, j, sum])
                 else:
                     continue
