39bf42a28184e5406ce8a494d580655f3c0a4e70
snailshaw <snailshaw@126.com>
2023-07-28 17:56:15 +0800

    0

--- kw2vec.py
+++ kw2vec.py
@@ -1,41 +1,9 @@
-from utils import get_embedding
 from sklearn.metrics.pairwise import cosine_similarity
-import torch
 from tqdm import tqdm
 # import pandas as pd
 import pickle
 import numpy as np
 import csv
-
-
-# 分割成10个kw
-def get_each_keyword(x):
-    x_list = x.split("#")
-    return x_list
-
-
-# 分别对每个新闻选取出来的10个关键字进行嵌入编码
-embedding = get_embedding()
-
-
-def embed_keyword(x):
-    x_embeddings = torch.zeros(10, 768)
-    i = 0
-    for item in x:
-        kw_embedding = torch.tensor(embedding.embed_query(item), dtype=float)
-        x_embeddings[i, :] = kw_embedding
-        i = i + 1
-    return x_embeddings
-
-
-def embed_corpus(kws):
-    # 把每个新闻的关键字嵌入保存到列表中
-    with torch.no_grad():
-        kw_embedding_list = []
-        for item in tqdm(kws):
-            kw_list = get_each_keyword(item)
-            kw_embedding_list.append(embed_keyword(kw_list))
-        print(len(kw_embedding_list))
 
 
 def build_view(save_path, kw_embedding_list, threshold, num):
