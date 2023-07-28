4a5453e0925d02fa66c4b275af30d0136a968cfa
snailshaw <snailshaw@126.com>
2023-07-28 16:48:22 +0800

    0

--- evt2vec.py
+++ evt2vec.py
@@ -3,9 +3,11 @@ from tqdm import tqdm
 from sklearn.metrics.pairwise import cosine_similarity
 import numpy as np
 import pickle
+import pandas as pd
 
 
 def build_view(save_path, kw_embedding_list, threshold, num):
+    print("正在构建图ing")
     with open(save_path, 'w', newline='') as csvfile:
         writer = csv.writer(csvfile)
         # 写入表头
@@ -23,9 +25,13 @@ def build_view(save_path, kw_embedding_list, threshold, num):
 
 
 if __name__ == '__main__':
-    threshold = 0.5
-    num = 2
-    save_path = f"thucnews/views/event_views_{threshold}_{num}.csv"
-    with open("thucnews/views/pkl/event_embeddings.pickle", "rb") as file:
-        kw_embedding_list = pickle.load(file=file)
-    build_view(save_path=save_path, kw_embedding_list=kw_embedding_list, threshold=threshold, num=num)
+    threshold = 0.6
+    num_list = [2, 3, 5, 7]
+    for num in num_list:
+        save_path = f"thucnews/t2v_large/event_views_{threshold}_{num}_large.csv"
+        with open("thucnews/t2v_large/pkl/event_embeddings_large.pickle", "rb") as file:
+            kw_embedding_list = pickle.load(file=file)
+        build_view(save_path=save_path, kw_embedding_list=kw_embedding_list, threshold=threshold, num=num)
+        print("正在清理文件ing")
+        df = pd.read_csv(save_path)
+        df.to_csv(save_path, sep=' ', index=False, header=False)
