70ffbe92f18b0632302ce9eeea4eef07612c280c
snailshaw <snailshaw@126.com>
2023-07-28 18:01:54 +0800

    0

--- kw2vec.py
+++ kw2vec.py
@@ -1,6 +1,6 @@
 from sklearn.metrics.pairwise import cosine_similarity
 from tqdm import tqdm
-# import pandas as pd
+import pandas as pd
 import pickle
 import numpy as np
 import csv
@@ -24,10 +24,14 @@ def build_view(save_path, kw_embedding_list, threshold, num):
 
 
 if __name__ == '__main__':
-    threshold = 0.7
-    num = 2
-    save_path = f"thucnews/views/keyword_views_{threshold}_{num}.csv"
-    print(save_path)
-    with open("thucnews/views/pkl/keyword_embeddings.pickle", "rb") as file:
-        kw_embedding_list = pickle.load(file=file)
-    build_view(save_path=save_path, kw_embedding_list=kw_embedding_list, threshold=threshold, num=num)
+    threshold = 0.6
+    num_list = [3, 5, 7]
+    for num in num_list:
+        save_path = f"thucnews/views/keyword_views_{threshold}_{num}.csv"
+        print(save_path)
+        with open("thucnews/views/pkl/keyword_embeddings.pickle", "rb") as file:
+            kw_embedding_list = pickle.load(file=file)
+        build_view(save_path=save_path, kw_embedding_list=kw_embedding_list, threshold=threshold, num=num)
+        print("正在清理文件ing")
+        df = pd.read_csv(save_path)
+        df.to_csv(save_path, sep=' ', index=False, header=False)
