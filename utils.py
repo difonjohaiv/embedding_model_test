4ca8e30b44f5e9b01ebc461aa9676ca17a0f92f6
snailshaw <snailshaw@126.com>
2023-09-10 10:33:51 +0800

    0

--- utils.py
+++ utils.py
@@ -1,21 +1,25 @@
 import torch
+import pickle
 from langchain.embeddings.huggingface import HuggingFaceEmbeddings
 from torch.nn import CosineSimilarity
 
-EMBEDDING_DEVICE = "cuda" if torch.cuda.is_available(
-) else "mps" if torch.backends.mps.is_available() else "cpu"
+print(torch.cuda.is_available())
+
+EMBEDDING_DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 model_kwargs = {'device': EMBEDDING_DEVICE}
 
 embedding_model_dict = {
     "text2vec-base": "models/text2vec-base-chinese",
     "text2vec-large": "models/text2vec-large-chinese",
     "m3e-base": "models/m3e-base",
-    "bge-large": "models/bge-large-zh"
+    "bge-large": "models/bge-large-zh",
+    "bge-large-en": "models/bge-large-en",
+    "bge-small-en": "models/bge-small-en"
 }
 
 
 # 获取嵌入模型
-def get_embedding(model_name="text2vec-large"):
+def get_embedding(model_name="bge-small-en"):
     embedding = HuggingFaceEmbeddings(
         model_name=embedding_model_dict[model_name], model_kwargs=model_kwargs)
     print("获取的嵌入模型是:", model_name)
@@ -26,14 +30,3 @@ def get_embedding(model_name="text2vec-large"):
 def get_simfunc():
     simfunc = CosineSimilarity(dim=0, eps=1e-6)
     return simfunc
-
-
-if __name__ == '__main__':
-    titles = ["苹果", "Apple", "华为"]
-    embedding = get_embedding(model_name="bge-large")
-    t_1 = torch.tensor(embedding.embed_query(titles[0]), dtype=float)
-    t_2 = torch.tensor(embedding.embed_query(titles[1]), dtype=float)
-    # score = get_simfunc(t_1, t_2)
-    tool = get_simfunc()
-    score = tool(t_1, t_2)
-    print(score)
